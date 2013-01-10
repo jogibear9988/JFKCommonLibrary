13f7ab5d0aac075239e1991698eb035615c77fcf
jkuehner <jochen.kuehner@kardex.com>
2013-01-10 08:22:10 +0100

    JFK CommonLibrary Updates...

---
+++ JFKCommonLibrary/ExtensionMethods/IEnumerableExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JFKCommonLibrary.ExtensionMethods
+{
+    public static class IEnumerableExtensions
+    {
+        public static string AsString(this IEnumerable<string> list, string seperator = ", ")
+        {
+            if (list == null || list.Count() == 0) return "";
+            var retVal = "";
+            foreach (var itm in list)
+            {
+                retVal += itm + seperator;
+            }
+
+            return retVal.Substring(0, retVal.Length - seperator.Length);
+        }
+    }
+}
